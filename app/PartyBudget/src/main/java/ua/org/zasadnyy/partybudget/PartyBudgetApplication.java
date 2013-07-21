cfa4cf8929a24b091d8948ca2a4b42a85a30e69a
Vitaliy Zasadnyy <zasadnyy.vitalik@gmail.com>
2013-07-21 19:21:54 +0300

    Moved abs and sherlock navigation drawer dependencies to remote repo. Generated DAO layer.

---
+++ app/PartyBudget/src/main/java/ua/org/zasadnyy/partybudget/PartyBudgetApplication.java
@@ -0,0 +1,9 @@
+package ua.org.zasadnyy.partybudget;
+
+import android.app.Application;
+
+/**
+ * Created by vitaliyzasadnyy on 21.07.13.
+ */
+public class PartyBudgetApplication extends Application {
+}
---
+++ app/PartyBudget/src/main/java/ua/org/zasadnyy/partybudget/model/PartyStatus.java
@@ -0,0 +1,10 @@
+package ua.org.zasadnyy.partybudget.model;
+
+/**
+ * Created by vitaliyzasadnyy on 21.07.13.
+ */
+public enum PartyStatus {
+
+    IN_PROGRESS, CHECKOUT, FINISHED
+
+}
